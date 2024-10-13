b6a22130781f38f80739852d85171d49d172afff
Raffy.XPS12 <ucesrsp@ucl.ac.uk>
2024-10-13 16:44:12 +0100

    modified to pull in recipes

--- hungaryHippowebsite.py
+++ hungaryHippowebsite.py
@@ -1,20 +1,20 @@
+import os
 from flask import Flask, render_template
+from HungryHippo import getRecipes
 
 app = Flask(__name__, static_folder='templates/recipe')  # Specify static folder location
 
 @app.route('/')
 def index():
-  recipe_categories = [
-    {"name": "Italian", "recipes": [
-      {"title": "Spaghetti Bolognese", "url": "recipe/example.html"},
-      {"title": "Pizza Margherita", "url": "example.html"}
-    ]},
-    {"name": "Asian", "recipes": [
-      {"title": "Pad Thai", "url": "recipe/example.html"},
-      {"title": "Chicken Curry", "url": "example.html"}
-    ]}
-  ]
-  return render_template('index.html', recipe_categories=recipe_categories)
+  csv_file = 'HungaryHippoRecipies.csv'
+  num_rows_to_pick = 3  # Adjust as needed
+
+  current_dir = os.path.dirname(__file__)
+  # Construct the path to the recipe folder relative to the current directory
+  recipe_folder = os.path.join(current_dir, 'templates')
+
+  recipe_data = getRecipes(csv_file, recipe_folder, num_rows_to_pick).get_recipe_data()
+  return render_template('index.html', recipe_data=recipe_data)
 
 if __name__ == '__main__':
   app.run(debug=True)
